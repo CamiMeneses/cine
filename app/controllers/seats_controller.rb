68e6ee85a4ef589da4c5a9eb1d1f204991a711f6
CamiMeneses <CamiMeneses@github.com>
2020-02-14 00:22:08 -0500

    Improving  movies and seats controller

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -2,12 +2,15 @@
 
 class SeatsController < ApplicationController
   def index
-    @event = Event.find(params[:event_id])
-    @event_capacity = @event.capacity
+    @event_capacity = event.capacity
     @seat = Seat.all
   end
 
   private
+
+  def event
+    @event ||= Event.find(params[:event_id])
+  end
 
   def seat_params
     params.require(:seat).permit(:status)
